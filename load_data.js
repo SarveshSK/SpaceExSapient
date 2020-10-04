b020aa631085156a768f693571254eaaebb0f970
Sarvesh Shrikanth <s.shrikanth@prowareness.com>
2020-10-04 12:56:57 +0530

    filter results

--- load_data.js
+++ load_data.js
@@ -5,6 +5,7 @@
 var serverData, global_search = { 'year': [], 'launch': [], 'landing': [] },
     all_years = ['2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020'],
     URLSearchParams = new URLSearchParams("?foo=1&bar=2");
+var stateObj = {};
 console.log(URLSearchParams.toString());
 
 function loadSpacExData() {
@@ -51,19 +52,20 @@ function bindClickEvents() {
 }
 
 function getSearchParams() {
+    const stateObj = {};
     let attribute_year, attribute_launch, attribute_landing
     let attribute_type = this.getAttribute("search-type");
     if (attribute_type == "year") {
         attribute_year = this.classList.toggle('btn-dark') ? this.getAttribute("value") : removeFromSearch('year', this.getAttribute("value"));
-        global_search['year'].push(attribute_year);
+        (attribute_year) ? global_search['year'].push(attribute_year): '';
         URLSearchParams.append('year', attribute_year);
     } else if (attribute_type == "launch") {
         attribute_launch = this.classList.toggle('btn-dark') ? this.getAttribute("value") : removeFromSearch('launch', this.getAttribute("value"));
-        global_search['launch'].push(attribute_launch);
+        (attribute_launch) ? global_search['launch'] = attribute_launch: null;
         URLSearchParams.append('launch', attribute_launch);
     } else if (attribute_type == "landing") {
         attribute_landing = this.classList.toggle('btn-dark') ? this.getAttribute("value") : removeFromSearch('landing', this.getAttribute("value"));
-        global_search['landing'].push(attribute_landing);
+        (attribute_landing) ? global_search['landing'] = attribute_landing: null;
         URLSearchParams.append('launch', attribute_landing);
     }
 
@@ -71,7 +73,12 @@ function getSearchParams() {
 };
 
 function removeFromSearch(key, value) {
-    global_search[key] = global_search[key].filter(function(e) { return e != value });
+    if (key == 'year') {
+        let filtered_array = global_search[key].filter(function(e) { return e != value });
+        global_search[key] = filtered_array;
+    } else {
+        global_search[key] = '';
+    }
     URLSearchParams.delete(key);
     filterResults();
 }
@@ -81,9 +88,16 @@ function filterResults() {
     launch_year = global_search['year'].length == 0 ? all_years : global_search['year'];
     launch_success = global_search['launch'];
     landing_success = global_search['landing'];
+    (launch_success == 'true') ? launch_success_t = true: launch_success_t = '';
+    (landing_success == 'true') ? landing_success_t = true: landing_success_t = '';
+    (launch_success == 'false') ? launch_success_f = false: launch_success_f = '';
+    (landing_success == 'false') ? landing_success_f = false: landing_success_f = '';
+
+    console.log(launch_success_t, launch_success_f);
+
     renderResults(serverData.filter(
         function(entry) {
-            return launch_year.includes((entry.launch_year).toString());
+            return launch_year.includes((entry.launch_year).toString()) && (entry.launch_success == launch_success_t);
         }
     ))
 
@@ -93,6 +107,8 @@ function readStoredURLParams() {
     global_search['year'] = URLSearchParams.getAll('year');
     global_search['launch'] = URLSearchParams.getAll('launch');
     global_search['landing'] = URLSearchParams.getAll('landing');
+
+
     if (global_search['year'].length > 0 || global_search['launch'].length > 0 || global_search['landing'].length > 0) {
         filterResults();
     }
